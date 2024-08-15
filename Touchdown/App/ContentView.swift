aaea0d7afcb59dbd6b967e5b431d74624e09510d
far1h <farihmuhammad45@gmail.com>
2024-08-15 11:39:53 +0700

    add adaptive carousel with page tab view

--- Touchdown/App/ContentView.swift
+++ Touchdown/App/ContentView.swift
@@ -21,9 +21,15 @@ struct ContentView: View {
 //                    .padding(.top, UIApplication.shared.windows.first?.safeAreaInsets.top)
                     .background(.white)
                     .shadow(color: .black.opacity(0.05), radius: 5, x: 0, y: 5)
-                Spacer()
-                FooterView()
-                    .padding(.horizontal)
+                ScrollView(.vertical, showsIndicators: false) {
+                    VStack(spacing: 0) {
+                        FeaturedTabView()
+                            .padding(.vertical,20)
+                            .frame(height: UIScreen.main.bounds.width / 1.475)
+                        FooterView()
+                            .padding(.horizontal)
+                    } //: VSTACK
+                } //: SCROLL
             } //: VSTACK
             .background(colorBackground.ignoresSafeArea())
         } //: ZSTACK
---
+++ Touchdown/Model/PlayerModel.swift
@@ -0,0 +1,13 @@
+//
+//  PlayerModel.swift
+//  Touchdown
+//
+//  Created by Farih Muhammad on 15/08/2024.
+//
+
+import Foundation
+
+struct Player: Codable, Identifiable {
+    let id: Int
+    let image: String
+}
